77455f38219e706d5a58e111665dea99ceaaecc0
Alan Nuari <alannuariii@gmail.com>
2026-01-07 07:33:34 +0800

    Update

---
+++ src/lib/data/engineData.js
@@ -0,0 +1,15 @@
+export const engines = () => [
+    { unit: 3, mesin: "DEUTZ  BV 8M 628" },
+    { unit: 4, mesin: "ABC  8E DZC - 750" },
+    { unit: 5, mesin: "DEUTZ  BV 8M 628" },
+    { unit: 6, mesin: "MTU 12 V 4000 G 61" },
+    { unit: 7, mesin: "Mitsubishi S16R PTA-S" },
+    { unit: 8, mesin: "Mitsubishi S16R PTA-S" },
+    { unit: 9, mesin: "Mitsubishi S16R PTA-S" },
+    { unit: 10, mesin: "Mitsubishi S16R PTA-S" },
+    { unit: 11, mesin: "CUMMINS  KTA-50-G8" },
+    { unit: 12, mesin: "CUMMINS  KTA-50-G8" },
+    { unit: 13, mesin: "CUMMINS  KTA-50-G8" },
+    { unit: 15, mesin: "CUMMINS  KTA-50-G8" },
+];
+
